e3806af9b495853c4584b80dde2d13b30e544651
Jason_Teng <sdonline13@gmail.com>
2023-07-16 13:52:14 +0800

    add jwt dependency

--- src/main/java/Project_PET_BackEnd/config/security/SecurityConfig.java
+++ src/main/java/Project_PET_BackEnd/config/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package Project_PET_BackEnd.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,5 +26,10 @@ public class SecurityConfig   extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
