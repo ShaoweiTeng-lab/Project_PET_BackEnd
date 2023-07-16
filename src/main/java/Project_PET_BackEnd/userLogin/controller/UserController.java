b9f9834735bb395d949e851429d99ba781af7abf
Jason_Teng <sdonline13@gmail.com>
2023-07-16 16:44:39 +0800

    add env.properties

--- src/main/java/Project_PET_BackEnd/userLogin/controller/UserController.java
+++ src/main/java/Project_PET_BackEnd/userLogin/controller/UserController.java
@@ -2,6 +2,7 @@ package Project_PET_BackEnd.userLogin.controller;
 
 import Project_PET_BackEnd.userLogin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,17 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
     @Autowired
     private UserService userService;
     @PostMapping("/user/login")
     public ResponseEntity<?> login(){
 
-        return  ResponseEntity.status(HttpStatus.OK).body("登入成功");
+        return  ResponseEntity.status(HttpStatus.OK).body("登入成功" );
     }
 
     @PostMapping("/user/profile")
     public ResponseEntity<?> getUserProfile(@RequestAttribute String userId){
 
-        return  ResponseEntity.status(HttpStatus.OK).body("登入成功");
+        return  ResponseEntity.status(HttpStatus.OK).body("使用者訊息");
     }
 }
