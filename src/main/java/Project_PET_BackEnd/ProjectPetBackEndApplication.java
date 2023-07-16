11310b7277993faf93724fb41df4eb2c00a6be4a
Jason_Teng <sdonline13@gmail.com>
2023-07-16 13:28:22 +0800

    add git

---
+++ src/main/java/Project_PET_BackEnd/ProjectPetBackEndApplication.java
@@ -0,0 +1,13 @@
+package Project_PET_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPetBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPetBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project_PET_BackEnd/ProjectPetBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Project_PET_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPetBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
