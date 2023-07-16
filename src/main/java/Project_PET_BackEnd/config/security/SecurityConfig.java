20232a94cdf8be5cfec6928c4d6cda5580aa3e4c
Jason_Teng <sdonline13@gmail.com>
2023-07-16 13:42:09 +0800

    add security

---
+++ src/main/java/Project_PET_BackEnd/config/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package Project_PET_BackEnd.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig   extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/users/login").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
