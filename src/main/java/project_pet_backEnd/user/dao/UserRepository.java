8ce06a549d514bed410942fec6ac0ff46548304d
Jason_Teng <sdonline13@gmail.com>
2023-07-31 22:16:23 +0800

    add userJpa

---
+++ src/main/java/project_pet_backEnd/user/dao/UserRepository.java
@@ -0,0 +1,7 @@
+package project_pet_backEnd.user.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project_pet_backEnd.user.vo.User;
+
+public interface UserRepository  extends JpaRepository<User,Integer> {
+}
--- src/main/java/project_pet_backEnd/user/vo/User.java
+++ src/main/java/project_pet_backEnd/user/vo/User.java
@@ -1,21 +1,43 @@
 package project_pet_backEnd.user.vo;
 
 import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
+import javax.persistence.*;
 import java.sql.Date;
+@DynamicInsert
+@DynamicUpdate
+@Entity
+@Table(name = "user")
 @Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
     private Integer userId;
+    @Column(name = "USER_NAME")
     private String  userName;
+    @Column(name = "USER_NICKNAME")
     private  String  userNickName;
+    @Column(name = "USER_GENDER")
     private Integer userGender;
+    @Column(name = "USER_EMAIL")
     private  String  userEmail;
+    @Column(name = "USER_PASSWORD")
     private String  userPassword;
+    @Column(name = "USER_PHONE")
     private String  userPhone;
+    @Column(name = "USER_ADDRESS")
     private  String userAddress;
-    private Date userBirthday;;//sql.date
+    @Column(name = "USER_BIRTHDAY")
+    private Date userBirthday;//sql.date
+    @Column(name = "USER_POINT", columnDefinition = "INT DEFAULT 0")
     private Integer userPoint;
+    @Column(name = "USER_PIC")
     private byte[] userPic;
+    @Column(name = "USER_PROVIDER")
     private IdentityProvider identityProvider;
+    @Column(name = "USER_CREATED", columnDefinition = "TIMESTAMP  DEFAULT CURRENT_TIMESTAMP")
     private java.util.Date userCreated;//util.date
 }
