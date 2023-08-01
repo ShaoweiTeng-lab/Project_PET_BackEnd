5bb1a6b38fae957c4b083468f173dc78744a3f76
Jorden <bacteriadark@gmail.com>
2023-08-01 21:13:55 +0800

    GOMMER DTO UPDATE

---
+++ src/main/java/project_pet_backEnd/groomer/dto/PetGroomerWorkManagement.java
@@ -0,0 +1,10 @@
+package project_pet_backEnd.groomer.dto;
+
+import lombok.Data;
+
+public class PetGroomerWorkManagement {
+    private String porTitle;
+    private String porText;
+    private String piPicture;
+    private Data proUpload;
+}
