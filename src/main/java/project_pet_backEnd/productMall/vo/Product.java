7a47840c8b90404becac9b55595663fb490fdc73
Huangstt <a2902511@gmail.com>
2023-08-02 18:52:17 +0800

    add Product VO

---
+++ src/main/java/project_pet_backEnd/productMall/vo/Product.java
@@ -0,0 +1,16 @@
+package project_pet_backEnd.productMall.vo;
+
+import lombok.Data;
+
+import java.sql.Date;
+@Data //自動讓所有屬性擁有getter/setter
+public class Product {
+    private Integer pdNo;
+    private String pdName;
+    private Integer pdPrice;
+    private String pdDescription;
+    private Date pdUpdate; //sql.date
+    private Integer pdStatus;
+    private Integer pdTotalreview;
+    private Integer pdScore;
+}
