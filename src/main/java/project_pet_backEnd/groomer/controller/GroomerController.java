c97a98ccaf3283a5695286e8e32423c83fa6bcf1
Yang-hung-fei <x52131221@gmail.com>
2023-08-02 18:44:30 +0800

    完成 1.PetGroomerService v2 2.PetGroomerServiceImp v3

--- src/main/java/project_pet_backEnd/groomer/controller/GroomerController.java
+++ src/main/java/project_pet_backEnd/groomer/controller/GroomerController.java
@@ -1,11 +1,19 @@
 package project_pet_backEnd.groomer.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project_pet_backEnd.groomer.service.PetGroomerService;
 
-@RequestMapping("/manager")
+@RestController
+@Validated
+
 public class GroomerController {
 
+    @Autowired
+    PetGroomerService petGroomerService;
 
 
 }
--- src/main/java/project_pet_backEnd/groomer/dto/PetGroomerInsertRequest.java
+++ src/main/java/project_pet_backEnd/groomer/dto/PetGroomerInsertRequest.java
@@ -3,12 +3,20 @@ package project_pet_backEnd.groomer.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.sql.Date;
 
 @Data
 public class PetGroomerInsertRequest {
+    @NotBlank
     private int manId;
+    @NotBlank
     private String pgName;
+    @Min(0)
+    @Max(1)
     private int pgGender;
     private String pgPic;//base64
     private String pgEmail;
