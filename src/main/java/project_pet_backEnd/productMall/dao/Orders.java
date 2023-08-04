c7874751740857b46683d3ecdcaa71a215bbcd86
yunyi <cloudy12374@gmail.com>
2023-08-05 01:42:53 +0800

    Add productMall VO Add productMall Dao Add  ordersController

---
+++ src/main/java/project_pet_backEnd/productMall/dao/Orders.java
@@ -0,0 +1,10 @@
+package project_pet_backEnd.productMall.dao;
+
+public interface Orders {
+    /**
+     * @使用者新增訂單
+     * @param orders
+     */
+    public abstract String insertOrders(Orders orders);
+
+}
