e7098c571a8ca4e9f387bf1de8798b84e7bfd1dd
Yang-hung-fei <x52131221@gmail.com>
2023-08-07 22:52:30 +0800

    ver1

--- src/main/java/project_pet_backEnd/groomer/appointment/service/imp/GroomerAppointmentServiceImp.java
+++ src/main/java/project_pet_backEnd/groomer/appointment/service/imp/GroomerAppointmentServiceImp.java
@@ -1,6 +1,7 @@
 package project_pet_backEnd.groomer.appointment.service.imp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import project_pet_backEnd.groomer.appointment.dto.response.GetAllGroomersForAppointmentRes;
 import project_pet_backEnd.groomer.appointment.service.GroomerAppointmentService;
@@ -9,48 +10,50 @@ import project_pet_backEnd.groomer.petgroomer.vo.PetGroomer;
 import project_pet_backEnd.groomer.petgroomerschedule.dao.PetGroomerScheduleDao;
 import project_pet_backEnd.groomer.petgroomerschedule.utils.AppointmentUtils;
 import project_pet_backEnd.groomer.petgroomerschedule.vo.PetGroomerSchedule;
-import project_pet_backEnd.redis.RedisService;
 import project_pet_backEnd.utils.AllDogCatUtils;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class GroomerAppointmentServiceImp implements GroomerAppointmentService {
-   @Autowired
-   PetGroomerDao petGroomerDao;
-   @Autowired
-   PetGroomerScheduleDao petGroomerScheduleDao;
+    @Autowired
+    PetGroomerDao petGroomerDao;
+    @Autowired
+    PetGroomerScheduleDao petGroomerScheduleDao;
 
-   @Autowired
-   AppointmentUtils appointmentUtils;
+    @Autowired
+    AppointmentUtils appointmentUtils;
 
-   @Autowired
-   RedisService redisService;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Override
     public List<GetAllGroomersForAppointmentRes> getAllGroomersForAppointment() {
+        String redisKey = "getAllGroomersForAppointmentRes";
 
-        List<Object> getAllGroomersForAppointmentRes = redisService.getCachedQueryResults("getAllGroomersForAppointmentRes");
+        List<Object> getAllGroomersForAppointmentRes = redisTemplate.opsForList().range(redisKey, 0, -1);
 
         if (getAllGroomersForAppointmentRes == null || getAllGroomersForAppointmentRes.isEmpty()) {
             // 查詢數據庫
             List<PetGroomer> allGroomer = petGroomerDao.getAllGroomer();
             // 將結果緩存到 Redis
-            redisService.cacheQueryResults(allGroomer, "getAllGroomersForAppointmentRes", 60);
-            List<GetAllGroomersForAppointmentRes> getAllGroomersForAppointmentResList= new ArrayList<>();
-            for(PetGroomer petGroomer:allGroomer){
+            redisTemplate.opsForList().leftPushAll(redisKey, allGroomer);
+            redisTemplate.expire(redisKey, 60, TimeUnit.MINUTES);
+
+            List<GetAllGroomersForAppointmentRes> getAllGroomersForAppointmentResList = new ArrayList<>();
+            for (PetGroomer petGroomer : allGroomer) {
                 GetAllGroomersForAppointmentRes res = new GetAllGroomersForAppointmentRes();
                 res.setPgId(petGroomer.getPgId());
                 res.setPgName(petGroomer.getPgName());
-                int gender=petGroomer.getPgGender();
-                switch (gender){
+                int gender = petGroomer.getPgGender();
+                switch (gender) {
                     case 0:
                         res.setPgGender("女性");
-                    break;
+                        break;
                     case 1:
                         res.setPgGender("男性");
-                    break;
+                        break;
                 }
                 res.setPgPic(AllDogCatUtils.base64Encode(petGroomer.getPgPic()));
                 getAllGroomersForAppointmentResList.add(res);
@@ -65,13 +68,14 @@ public class GroomerAppointmentServiceImp implements GroomerAppointmentService {
                     groomers.add((PetGroomer) obj);
                 }
             }
-            List<GetAllGroomersForAppointmentRes> getAllGroomersForAppointmentResList= new ArrayList<>();
-            for(PetGroomer petGroomer : groomers){
+
+            List<GetAllGroomersForAppointmentRes> getAllGroomersForAppointmentResList = new ArrayList<>();
+            for (PetGroomer petGroomer : groomers) {
                 GetAllGroomersForAppointmentRes res = new GetAllGroomersForAppointmentRes();
                 res.setPgId(petGroomer.getPgId());
                 res.setPgName(petGroomer.getPgName());
-                int gender=petGroomer.getPgGender();
-                switch (gender){
+                int gender = petGroomer.getPgGender();
+                switch (gender) {
                     case 0:
                         res.setPgGender("女性");
                         break;
@@ -86,8 +90,6 @@ public class GroomerAppointmentServiceImp implements GroomerAppointmentService {
         }
     }
 
-    //
-
     /*
      *配合getAllGroomersForAppointment 需在前端自帶隱含input 有pgId來使用
      * 取得當前伺服器(含當天)一個月內的該pg班表
@@ -96,19 +98,19 @@ public class GroomerAppointmentServiceImp implements GroomerAppointmentService {
     public List<PetGroomerSchedule> getGroomerScheduleByPgId(Integer pgId) {
         String redisKey = "pgschedules_" + pgId;
 
-        List<Object> cachedSchedule = redisService.getCachedQueryResults(redisKey);
-
+        List<Object> cachedSchedule = redisTemplate.opsForList().range(redisKey, 0, -1);
         List<PetGroomerSchedule> pgScheduleByPgIdList;
-        // 檢查 Redis 中是否已經緩存了相應的 Schedule
+
         if (cachedSchedule != null && !cachedSchedule.isEmpty()) {
             // 如果 Redis 中有緩存，直接從緩存中取出結果
-            pgScheduleByPgIdList = appointmentUtils.convertToPetGroomerScheduleList(cachedSchedule);
+            pgScheduleByPgIdList = convertToPetGroomerScheduleList(cachedSchedule);
         } else {
             // 如果 Redis 中沒有緩存，從數據庫中查詢 Schedule，並過濾出當前伺服器時間(含當日凌晨開始)至未來一個月內的 Schedule
-            pgScheduleByPgIdList = appointmentUtils.filterFutureSchedules(petGroomerScheduleDao.getPgScheduleByPgId(pgId));
+            pgScheduleByPgIdList = filterFutureSchedules(petGroomerScheduleDao.getPgScheduleByPgId(pgId));
 
             // 將結果緩存到 Redis，設定過期時間為 1 小時
-            redisService.cacheQueryResults(pgScheduleByPgIdList, redisKey, 60);
+            redisTemplate.opsForList().leftPushAll(redisKey, pgScheduleByPgIdList);
+            redisTemplate.expire(redisKey, 60, TimeUnit.MINUTES);
         }
         return pgScheduleByPgIdList;
     }
