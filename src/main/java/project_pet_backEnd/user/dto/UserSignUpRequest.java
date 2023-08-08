f65ec272c0b731bb3c78ddc6daa3cde338d4502d
Jason_Teng <sdonline13@gmail.com>
2023-08-08 11:29:30 +0800

    feature: api文件隱藏註冊屬性

--- src/main/java/project_pet_backEnd/user/dto/UserSignUpRequest.java
+++ src/main/java/project_pet_backEnd/user/dto/UserSignUpRequest.java
@@ -1,5 +1,7 @@
 package project_pet_backEnd.user.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.Data;
 import project_pet_backEnd.user.vo.IdentityProvider;
 
@@ -24,7 +26,9 @@ public class UserSignUpRequest {
     private String  userPhone;
     @NotBlank
     private  String userAddress;
+    @ApiModelProperty(value = "Hidden property", hidden = true)
     private  byte[] userPic;
+    @ApiModelProperty(value = "Hidden property", hidden = true)
     private Date userBirthday;
     private IdentityProvider identityProvider;
     @NotBlank
