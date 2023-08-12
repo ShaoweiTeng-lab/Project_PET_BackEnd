13457002b520d514d8a3ab03fa3e5f121972cf54
Jason_Teng <sdonline13@gmail.com>
2023-08-12 22:20:55 +0800

    feat: function 加入 vo

--- src/main/java/project_pet_backEnd/manager/vo/Function.java
+++ src/main/java/project_pet_backEnd/manager/vo/Function.java
@@ -3,9 +3,15 @@ package project_pet_backEnd.manager.vo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
 @Data
 @AllArgsConstructor
+@Entity
 public class Function {
+    @Column(name = "function_Id")
     private  Integer functionId;
+    @Column(name ="function_name")
     private  String functionName;
 }
