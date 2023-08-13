b986fbbe4b0395f56a51ff5276ef9b8364ea8fa9
yunyi <cloudy12374@gmail.com>
2023-08-13 10:01:52 +0800

    修正OrdersVO檔案路徑

--- src/main/java/project_pet_backEnd/productMall/order/vo/OrderList.java
+++ src/main/java/project_pet_backEnd/productMall/order/vo/OrderList.java
@@ -3,7 +3,7 @@ package project_pet_backEnd.productMall.order.vo;
 import lombok.Data;
 import java.io.Serializable;
 
-@Data
+@Data //生成符合 Java Bean getter setter 無參建構子
 public class OrderList implements Serializable {
     private Integer ordNo; //訂單編號 {pk,fk}
     private Integer pdNo;  //商品編號 {pk,fk}
