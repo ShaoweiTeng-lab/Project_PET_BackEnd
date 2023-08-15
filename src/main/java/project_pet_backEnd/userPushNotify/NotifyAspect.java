3d6211e26c14d2352fb81f1222b98f176f11acb3
Jason_Teng <sdonline13@gmail.com>
2023-08-15 20:17:52 +0800

    feat:新增 Notify Aop

--- src/main/java/project_pet_backEnd/userPushNotify/NotifyAspect.java
+++ src/main/java/project_pet_backEnd/userPushNotify/NotifyAspect.java
@@ -1,13 +1,18 @@
 package project_pet_backEnd.userPushNotify;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
-public class NotifyAspect {
-    @Autowired
-    private  UserNotifyWebSocketHandler userNotifyWebSocketHandler;
-
-}
+//@Aspect
+//@Component
+//public class NotifyAspect {
+//    @Autowired
+//    private  UserNotifyWebSocketHandler userNotifyWebSocketHandler;
+//    @After("execution(* project_pet_backEnd.groomer.*(..))")
+//    public  void  groomerUpdateNotify(){
+//
+//    }
+//
+//}
