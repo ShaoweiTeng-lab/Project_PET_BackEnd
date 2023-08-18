fc74f6df6723281346d4ea3836bc99f37560ee1e
Yang-hung-fei <x52131221@gmail.com>
2023-08-18 14:27:41 +0800

    PG SC ADD

--- src/main/java/project_pet_backEnd/groomer/petgroomerschedule/service/groomerScheduleService.java
+++ src/main/java/project_pet_backEnd/groomer/petgroomerschedule/service/groomerScheduleService.java
@@ -1,6 +1,12 @@
 package project_pet_backEnd.groomer.petgroomerschedule.service;
 
+import project_pet_backEnd.groomer.petgroomerschedule.dto.response.GetScheduleRes;
+import java.util.List;
+
 public interface groomerScheduleService {
+
+    //按月份取得班表 for Man
+    List<GetScheduleRes> getMonthScheduleForMan();
 
 
 
