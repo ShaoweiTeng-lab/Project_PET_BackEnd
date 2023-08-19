70541f14c011375240c2e29488cc373e530d1282
Yang-hung-fei <x52131221@gmail.com>
2023-08-19 16:21:25 +0800

    Revert "PG SC ADD"
    This reverts commit 2d5d89e439357e8655ff7673896ddda2dde2d662.

--- src/main/java/project_pet_backEnd/groomer/petgroomerschedule/service/GroomerScheduleService.java
+++ src/main/java/project_pet_backEnd/groomer/petgroomerschedule/service/GroomerScheduleService.java
@@ -1,14 +1,13 @@
 package project_pet_backEnd.groomer.petgroomerschedule.service;
 
+import project_pet_backEnd.groomer.petgroomer.dto.response.GetAllGroomerListSortResForUser;
 import project_pet_backEnd.groomer.petgroomerschedule.dto.response.GetScheduleRes;
-import project_pet_backEnd.groomer.petgroomerschedule.dto.response.ListForScheduleRes;
-
 import java.util.List;
 
 public interface GroomerScheduleService {
 
     //取得美容師清單for 班表
-    List<ListForScheduleRes> getAllGroomerForSchedule();
+    List<GetAllGroomerListSortResForUser> getAllGroomerForSchedule();
 
 
     //按月份取得班表 for Man
--- src/main/java/project_pet_backEnd/groomer/petgroomerschedule/service/imp/GroomerScheduleServiceImp.java
+++ src/main/java/project_pet_backEnd/groomer/petgroomerschedule/service/imp/GroomerScheduleServiceImp.java
@@ -6,14 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import project_pet_backEnd.groomer.petgroomer.dao.PetGroomerDao;
+import project_pet_backEnd.groomer.petgroomer.dto.response.GetAllGroomerListSortResForUser;
 import project_pet_backEnd.groomer.petgroomer.vo.PetGroomer;
 import project_pet_backEnd.groomer.petgroomerschedule.dao.PetScheduleRepository;
 import project_pet_backEnd.groomer.petgroomerschedule.dto.response.GetScheduleRes;
-import project_pet_backEnd.groomer.petgroomerschedule.dto.response.ListForScheduleRes;
 import project_pet_backEnd.groomer.petgroomerschedule.service.GroomerScheduleService;
-import project_pet_backEnd.utils.AllDogCatUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,23 +23,11 @@ public class GroomerScheduleServiceImp implements GroomerScheduleService {
     PetGroomerDao petGroomerDao;
 
     @Override
-    public List<ListForScheduleRes> getAllGroomerForSchedule() {
+    public List<GetAllGroomerListSortResForUser> getAllGroomerForSchedule() {
         List<PetGroomer> allGroomer = petGroomerDao.getAllGroomer();
-        if(allGroomer==null)
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "未找到任一美容師");
 
-        List<ListForScheduleRes> getAllGroomerList = new ArrayList<>();
 
-        for(PetGroomer groomer : allGroomer){
-            ListForScheduleRes resList = new ListForScheduleRes();
-
-            resList.setPgId(groomer.getPgId());
-            resList.setPgName(groomer.getPgName());
-            resList.setPgPic(AllDogCatUtils.base64Encode(groomer.getPgPic()));
-            getAllGroomerList.add(resList);
-        }
-
-        return getAllGroomerList;
+        return null;
     }
 
     @Override
