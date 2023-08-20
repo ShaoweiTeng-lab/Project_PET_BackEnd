b8c7578a37c5f197f11ad744d75b010b723a4ad7
CHEN YI TING <abc10512106@gmail.com>
2023-08-20 18:58:45 +0800

    add post collection controller architecture

---
+++ src/main/java/project_pet_backEnd/socialMedia/postCollection/vo/PostCol.java
@@ -0,0 +1,25 @@
+package project_pet_backEnd.socialMedia.postCollection.vo;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "post_collect")
+public class PostCol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PC_ID")
+    Integer pcId;
+    @Column(name = "USER_ID")
+    Integer userId;
+    @Column(name = "POST_ID")
+    Integer postId;
+    @CreationTimestamp
+    @Column(name = "PC_CTIME")
+    Timestamp createTime;
+
+}
