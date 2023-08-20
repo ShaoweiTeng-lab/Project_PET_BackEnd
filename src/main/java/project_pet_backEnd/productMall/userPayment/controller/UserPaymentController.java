53dd7196f6ef51e1f17bc8bd72e879a8faab8188
Jason_Teng <sdonline13@gmail.com>
2023-08-20 21:40:07 +0800

    feat:第三方支付

---
+++ src/main/java/project_pet_backEnd/productMall/userPayment/controller/UserPaymentController.java
@@ -0,0 +1,34 @@
+package project_pet_backEnd.productMall.userPayment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project_pet_backEnd.ecpay.demo.dto.OrderRequest;
+import project_pet_backEnd.ecpay.demo.dto.OrderResponse;
+import project_pet_backEnd.productMall.userPayment.service.UserPaymentService;
+import project_pet_backEnd.utils.commonDto.ResultResponse;
+@RequestMapping("/user")
+@RestController
+public class UserPaymentController {
+    @Autowired
+    private UserPaymentService userPaymentService;
+
+    /**
+     * 得到渲染後的form表單
+     * */
+    @GetMapping("/productMall/order")
+    public ResultResponse<String> getPaymentForm(@RequestAttribute Integer userId, @RequestParam Integer orderId){
+        ResultResponse rs =new ResultResponse();
+        String  form =userPaymentService.getPaymentForm(userId,orderId);
+        rs.setMessage(form);
+        return  rs;
+    }
+
+    @PostMapping("/successPay/{orderId}")
+    public ResponseEntity<String> successPayCallBack(@PathVariable("orderId") String orderId){
+        userPaymentService.successPayCallBack(orderId);
+        return  ResponseEntity.status(HttpStatus.CREATED).body("付款成功");
+
+    }
+}
