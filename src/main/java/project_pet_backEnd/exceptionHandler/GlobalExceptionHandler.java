409a475a38a0643789eeda76ebcdd8dd5c7b00e3
Jason_Teng <sdonline13@gmail.com>
2023-08-22 16:36:07 +0800

    feat:更改 GlobalExceptionHandler

--- src/main/java/project_pet_backEnd/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/project_pet_backEnd/exceptionHandler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package project_pet_backEnd.exceptionHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,7 +16,7 @@ import javax.validation.ConstraintViolationException;
  * 全域 異常處理 防止部分驗證回傳 500
  */
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler  {
 
     /**
      * 處理丟出的 ResponseStatusException
@@ -31,14 +32,28 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(rs, HttpStatus.OK);
     }
 
+//    /**
+//     * 驗證參數異常
+//     */
+//    @ExceptionHandler(ConstraintViolationException.class)
+//    public ResponseEntity<?> handleRequestValidError(ConstraintViolationException ex) throws JsonProcessingException {
+//        // 返回錯誤訊息和 400 Bad Request 狀態碼
+//        ResultResponse rs = new ResultResponse();
+//        String msg = ex.getMessage();
+//        rs.setMessage(msg);
+//        rs.setCode(400);//改成將異常code丟入訊息中
+//        return new ResponseEntity<>(rs, HttpStatus.OK);
+//    }
+
     /**
      * 驗證參數異常
      */
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<?> handleRequestValidError(ConstraintViolationException ex) throws JsonProcessingException {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleRequestValidError(MethodArgumentNotValidException  ex) throws JsonProcessingException {
         // 返回錯誤訊息和 400 Bad Request 狀態碼
         ResultResponse rs = new ResultResponse();
-        rs.setMessage("使用者輸入異常");
+        String msg = ex.getMessage();
+        rs.setMessage(msg);
         rs.setCode(400);//改成將異常code丟入訊息中
         return new ResponseEntity<>(rs, HttpStatus.OK);
     }
