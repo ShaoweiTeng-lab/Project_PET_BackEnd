6f95379a8e99daf6c9b3a5fbc4bc63c9ebab8313
Jason_Teng <sdonline13@gmail.com>
2023-08-23 09:27:08 +0800

    feat:修改 userNotify

--- src/main/java/project_pet_backEnd/userPushNotify/NotifyMsg.java
+++ src/main/java/project_pet_backEnd/userPushNotify/NotifyMsg.java
@@ -2,12 +2,13 @@ package project_pet_backEnd.userPushNotify;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NotifyMsg {
     private  NotifyType notifyType;
     private  String image;
     private  String msg;
-    public  NotifyMsg(){};
 }
