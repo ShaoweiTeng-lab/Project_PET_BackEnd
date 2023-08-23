63f4672486fd275c5d2bbe3df74711d7c529fb27
Jason_Teng <sdonline13@gmail.com>
2023-08-23 09:52:25 +0800

    feat : 完成美容師發布作品時 發布通知

--- src/main/java/project_pet_backEnd/userPushNotify/NotifyAspect.java
+++ src/main/java/project_pet_backEnd/userPushNotify/NotifyAspect.java
@@ -5,18 +5,23 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import project_pet_backEnd.userPushNotify.dao.PictureInfoRepository;
+import project_pet_backEnd.userPushNotify.vo.PictureInfo;
+import project_pet_backEnd.utils.AllDogCatUtils;
 
 @Aspect
 @Component
 public class NotifyAspect {
     @Autowired
+    private PictureInfoRepository pictureInfoRepository;
+    @Autowired
     private  UserNotifyWebSocketHandler userNotifyWebSocketHandler;
     //To Do: 美容師 新增作品時
-    @After("execution(* project_pet_backEnd.user.controller.*.*(..))")
+    @After("execution(* project_pet_backEnd.groomer.groomerworkmanager.service.*.insert(..))")
     public  void  groomerUpdateNotify() throws Exception {
              NotifyType notifyType =NotifyType.Groomer;
-             NotifyMsg notifyMsg =new NotifyMsg(notifyType,null,"有美容師發布作品了，趕快來看看喔~");
-             System.out.println("執行 groomerUpdateNotify");
+             PictureInfo pictureInfo= pictureInfoRepository.findFirstByOrderByPiDateDesc();
+             NotifyMsg notifyMsg =new NotifyMsg(notifyType, AllDogCatUtils.base64Encode(pictureInfo.getPiPicture()),"有美容師發布作品了，趕快來看看喔~");
              userNotifyWebSocketHandler.publicNotifyMsg(notifyMsg);
     }
     //To Do: 商城 新增商品時
@@ -24,8 +29,7 @@ public class NotifyAspect {
     public  void  productUpdateNotify() throws Exception {
         NotifyType notifyType =NotifyType.Store;
         NotifyMsg notifyMsg =new NotifyMsg(notifyType,null,"商城有新的商品，趕快來看看喔~");
-        System.out.println("執行 groomerUpdateNotify");
-        userNotifyWebSocketHandler.publicNotifyMsg(notifyMsg);
+        System.out.println("執行 groomerUpdateNotify");userNotifyWebSocketHandler.publicNotifyMsg(notifyMsg);
     }
 
 }
