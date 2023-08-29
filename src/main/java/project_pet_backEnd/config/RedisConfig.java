5275219fb0ccd3d77304946b38eea347020ff6a8
Jason_Teng <sdonline13@gmail.com>
2023-08-29 09:38:33 +0800

    fix: redis config

--- src/main/java/project_pet_backEnd/config/RedisConfig.java
+++ src/main/java/project_pet_backEnd/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package project_pet_backEnd.productMall.order.controller;
+package project_pet_backEnd.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
