2bf6ffc080e5764189d4974ee5d99dbeb9067e61
Jason_Teng <sdonline13@gmail.com>
2023-09-03 11:11:52 +0800

    feat:新增活動監聽

--- src/main/java/project_pet_backEnd/userPushNotify/NotifyAspect.java
+++ src/main/java/project_pet_backEnd/userPushNotify/NotifyAspect.java
@@ -6,6 +6,8 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import project_pet_backEnd.socialMedia.activityManager.dao.ActivityDao;
+import project_pet_backEnd.socialMedia.activityManager.vo.Activity;
+import project_pet_backEnd.userPushNotify.dao.ActivityRepository;
 import project_pet_backEnd.userPushNotify.dao.PictureInfoRepository;
 import project_pet_backEnd.userPushNotify.vo.PictureInfo;
 import project_pet_backEnd.utils.AllDogCatUtils;
@@ -16,7 +18,7 @@ public class NotifyAspect {
     @Autowired
     private PictureInfoRepository pictureInfoRepository;
     @Autowired
-    private ActivityDao activityDao;
+    private ActivityRepository activityRepository;
     @Autowired
     private  UserNotifyWebSocketHandler userNotifyWebSocketHandler;
     //todo: 美容師 新增作品時
@@ -34,15 +36,14 @@ public class NotifyAspect {
         NotifyMsg notifyMsg =new NotifyMsg(notifyType,null,"商城有新的商品，趕快來看看喔~");
         userNotifyWebSocketHandler.publicNotifyMsg(notifyMsg);
         // System.out.println("執行 groomerUpdateNotify");
-        // userNotifyWebSocketHandler.publicNotifyMsg(notifyMsg);
     }
 
-    //todo: 社群 新增貼文時
-    @After("execution(* project_pet_backEnd.productMall.productsmanage.service.*.insertProduct(..))")
+    //todo: 社群 新增活動時
+    @After("execution(* project_pet_backEnd.socialMedia.activityManager.service.*.create(..))")
     public  void  socialMediaUpdateNotify() throws Exception {
-        NotifyType notifyType =NotifyType.Store;
-        //activityDao.findFirstByOrderByPiDateDesc();
-        NotifyMsg notifyMsg =new NotifyMsg(notifyType,null,"商城有新的商品，趕快來看看喔~");
+        NotifyType notifyType =NotifyType.Activity;
+        Activity activity= activityRepository.findTopByOrderByActivityIdDesc();
+        NotifyMsg notifyMsg =new NotifyMsg(notifyType,AllDogCatUtils.base64Encode(activity.getActivityPicture()),"商城有新的商品，趕快來看看喔~");
         userNotifyWebSocketHandler.publicNotifyMsg(notifyMsg);
         // System.out.println("執行 groomerUpdateNotify");userNotifyWebSocketHandler.publicNotifyMsg(notifyMsg);
     }
