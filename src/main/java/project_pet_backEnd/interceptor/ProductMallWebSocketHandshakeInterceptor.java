a11b16125ee4f7592e114794247f060606006f01
Jason_Teng <sdonline13@gmail.com>
2023-09-07 11:50:22 +0800

    feat: 完成商城聊天室後端

--- src/main/java/project_pet_backEnd/interceptor/ProductMallWebSocketHandshakeInterceptor.java
+++ src/main/java/project_pet_backEnd/interceptor/ProductMallWebSocketHandshakeInterceptor.java
@@ -1,20 +1,29 @@
 package project_pet_backEnd.interceptor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+import project_pet_backEnd.manager.security.ManagerDetailsImp;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Map;
 
 @Component
 public class ProductMallWebSocketHandshakeInterceptor extends HttpSessionHandshakeInterceptor {
     @Autowired
     private WebsocketIdentityValid websocketIdentityValid;
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+    @Autowired
+    private ObjectMapper objectMapper;
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
                                    Map<String, Object> attributes) throws Exception {
@@ -24,6 +33,8 @@ public class ProductMallWebSocketHandshakeInterceptor extends HttpSessionHandsha
         if(attribute==null)
             return  false;
         //todo 判斷Manager 的token 是否具備 商城管理員權限
+        if(!validManagerPermission(attribute))
+            return  false;
         attributes.putAll(attribute);
         return  super.beforeHandshake(request, response, wsHandler, attributes);
     }
@@ -33,5 +44,20 @@ public class ProductMallWebSocketHandshakeInterceptor extends HttpSessionHandsha
 
     }
 
+    boolean validManagerPermission(Map<String ,Object>attribute) throws JsonProcessingException {
+
+        String connect =(String) attribute.get("connect");
+        if(!connect.contains("managerId"))//如果是user 放行
+            return  true;
+        Integer managerId=Integer.parseInt( connect.split("_")[1]);
+        String managerLoginJson=redisTemplate.opsForValue().get("Manager:Login:"+managerId);
+        ManagerDetailsImp managerDetail=null;
+        //判斷權限有無 商城管理
+        managerDetail=objectMapper.readValue(managerLoginJson,ManagerDetailsImp.class);
+        List<String> permissionList= managerDetail.getPermissionsList();
+        if(permissionList.contains("商城管理"))
+            return  true;
+        return  false;
+    }
 
 }
