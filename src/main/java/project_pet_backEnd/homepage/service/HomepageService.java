6fd0a637227bbb4a4c34afeb4d43a488501b3b2f
Kao <snoopy21024@yahoo.com.tw>
2023-09-15 19:36:37 +0800

    feature: activity rotes

--- src/main/java/project_pet_backEnd/homepage/service/HomepageService.java
+++ src/main/java/project_pet_backEnd/homepage/service/HomepageService.java
@@ -1,6 +1,8 @@
 package project_pet_backEnd.homepage.service;
 
+import project_pet_backEnd.homepage.dto.AcNewRes;
 import project_pet_backEnd.homepage.vo.NewsPic;
+import project_pet_backEnd.socialMedia.activityManager.dto.ActivityRes;
 import project_pet_backEnd.utils.commonDto.ResultResponse;
 
 import java.util.List;
@@ -8,7 +10,12 @@ import java.util.List;
 
 public interface HomepageService {
     String getGoogleMapApiKey();
+
     List<String> getRotePic();
+
     ResultResponse getNews();
+
     List<NewsPic> getNewsPic();
+
+    ResultResponse<List<AcNewRes>> getNewsActivities();
 }
